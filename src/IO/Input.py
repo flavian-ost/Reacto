ecdd305374f99a6f8f2b01684b38348709caa57e
Flavian <flavian.diethelm@ost.ch>
2025-11-18 11:57:15 +0100

    Expanded IO classes

--- src/IO/Input.py
+++ src/IO/Input.py
@@ -1,3 +1,39 @@
+from modulino import ModulinoKnob, ModulinoMovement, ModulinoButtons
+
 class Input:
+    button = []
+    knob = 0
+    acceleration = []
+    rotation = []
+
     def get_input():
         print("reading input...")
+
+    def get_button(which):
+        match which:
+            case "side":
+                return button[0]
+            case "left":
+                return button[1]
+            case "center":
+                return button[2]
+            case "right":
+                return button[3]
+    
+    def get_acceleration(which):
+        match which:
+            case "x":
+                return acceleration[0]
+            case "y":
+                return acceleration[1]
+            case "z":
+                return acceleration[2]
+    
+    def get_rotation(which):
+        match which:
+            case "x":
+                return rotation[0]
+            case "y":
+                return rotation[1]
+            case "z":
+                return rotation[2]
--- src/IO/Output.py
+++ src/IO/Output.py
@@ -1,3 +1,13 @@
+from modulino import ModulinoPixels, ModulinoBuzzer
+from lcd_i2c import LCD
+from machine import I2C
+
+display = LCD(0x27, 16, 2, i2c=I2C(1))
+display.begin()
+
 class Output:
     def set_output():
         print("setting output...")
+    
+    def display_message(message):
+        display.print(message)
